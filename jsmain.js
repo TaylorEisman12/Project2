f8f85ab26dcd7dc2e305ce0abca0cca29ea34c43
TaylorEisman12 <taylor@theeismans.com>
2012-03-10 17:10:35 -0600

    started my javascript file add window event listener, created function to get element, and make cats function

--- jsmain.js
+++ jsmain.js
@@ -1,4 +1,25 @@
 // Taylor Eisman
-// Project 2 
-// Jsmain.js
+// JavaScript File Project 2
+// VFW 03/12
 
+window.addEventListener("DOMContentLoaded", function(){
+
+  function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+
+	function makeCats(){
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = $('select'),
+			makeSelect = document.createElement('select');
+		makeSelect.setAttribute("id", "years");
+		for( var i=0, j=contactYears.length; i < j; i++ ) {
+			var makeOption = document.createElement("option");
+			var	optText = contactYears[i];
+			makeOption.setAttribute("value", contactYears[i]);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
+	}
